205d98f6ee9c41aa97bb0f709e016769c3c2d241
Administrator <Administrator@Lieber>
2016-03-06 16:50:24 +0800

    TEST

--- src/AppDemo.java
+++ src/AppDemo.java
@@ -1,56 +1,52 @@
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
 
-
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.io.File;
+import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.List;
 
 public class AppDemo {
-	private AppiumDriver driver;
+	private static AppiumDriver driver;
 
-	@Before
-	public void setUp() throws Exception {
-		// set up appium
+	public static void main(String[] args) throws MalformedURLException {
+
+		// 设置apk的路径
 		File classpathRoot = new File(System.getProperty("user.dir"));
 		File appDir = new File(classpathRoot, "apps");
 		File app = new File(appDir, "ISDAcceptance.apk");
+
+		// 设置自动化相关参数
 		DesiredCapabilities capabilities = new DesiredCapabilities();
 		capabilities.setCapability(CapabilityType.BROWSER_NAME, "");
+
+		// 测试手机操作系统
 		capabilities.setCapability("platformName", "Android");
-		capabilities.setCapability("deviceName", "Android Emulator");
+
+		// 模拟器类型
+		// capabilities.setCapability("deviceName", "582af7877b5a");
+		capabilities.setCapability("deviceName", "76P4C15708006345");
+		// 设置安卓系统版本
 		capabilities.setCapability("platformVersion", "4.4");
+
+		// 设置apk路径
 		capabilities.setCapability("app", app.getAbsolutePath());
-	//	capabilities.setCapability("appPackage",
-		//		"com.example.android.contactmanager");
-		capabilities.setCapability("appPackage",
-				"com.hw.smart.isdacceptance");
-		capabilities.setCapability("appActivity", ".LoginActivity");
-		driver = new AppiumDriver(new URL("http://127.0.0.1:4723/wd/hub"),
-				capabilities);
-	}
+		// 设置udid
+		// capabilities.setCapability("udid", "76P4C15708006345");
 
-	@After
-	public void tearDown() throws Exception {
+		// 设置app的主包名和主类包
+		capabilities.setCapability("appPackage", "com.hw.smart.isdacceptance");
+		capabilities.setCapability("appActivity", "com.hw.smart.isdacceptance.provider.activites.WelcomeActivity");
+		capabilities.setCapability("appWaitActivity", "com.hw.smart.isdacceptance.provider.activites.LoginActivity");// 初始化AppiumDriver
+
+		driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+
+		// 开始操作
+		driver.findElementById("et_login_uid").sendKeys("isdp1");
+ 
+
 		driver.quit();
-	}
-
-	@Test
-	public void addContact() {
-		WebElement el = driver.findElement(By.name("Add Contact"));
-		el.click();
-		List<WebElement> textFieldsList = driver
-				.findElementsByClassName("android.widget.EditText");
-		textFieldsList.get(0).sendKeys("Some Name");
-		textFieldsList.get(2).sendKeys("Some@example.com");
-		driver.swipe(100, 500, 100, 100, 2);
-		driver.findElementByName("Save").click();
 	}
 }
