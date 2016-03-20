976dc229b519c749f8e4134efcca22898c1b2c70
Administrator <Administrator@VZFP1J8K3E39W4G>
2016-03-20 22:42:24 +0800

    test

---
+++ src/com/lieber/features/Login.java
@@ -0,0 +1,32 @@
+package com.lieber.features;
+
+ 
+
+import org.testng.annotations.Test;
+
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class Login {
+
+	@Before
+	@Given("^打开应用$")
+	public void test1() {
+
+	}
+
+	@Test
+	@When("^输入账号密码，点击登录$")
+	public void test2() {
+
+	}
+
+	@After
+	@Then("^检测是否执行成功$")
+	public void test3() {
+
+	}
+}
