c08721607a97f0589a9c7fc36ba2d5d0e1fc6114
Administrator <Administrator@VZFP1J8K3E39W4G>
2016-04-04 23:12:01 +0800

    test

--- src/com/lieber/test/Test.java
+++ src/com/lieber/test/Test.java
@@ -2,10 +2,14 @@ package com.lieber.test;
 
 import com.lieber.tools.LieberTools;
 
+import junit.textui.TestRunner;
+
 public class Test {
 	public static void main(String[] args) {
 		int a[] = new int[] { 1, 4, 7, 8, 9 };
 		String str = LieberTools.CalculateScreenPoint(a, 44, 676, 640, 1272);
 		System.out.println(str);
+		
+		
 	}
 }
